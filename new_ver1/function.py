5c6457dcb93a4f7d8b485b344fc9a3d5ddfbe70e
ycx-git <2100011610@stu.pku.edu.cn>
2024-11-26 13:04:31 +0800

    .

--- new_ver1/function.py
+++ new_ver1/function.py
@@ -5,18 +5,17 @@ def fun1(start_value):
     end_value=start_value/(1-start_value)
     return end_value
 
-def fun2(x):
-    return x+1
+def fun2(f0):
+    return f0+1
 
-def fun3(x):
-    return x*np.e
+def fun3(f0):
+    return f0*np.e
 
-def fun4(x):
+def fun4(f0):
+    return 100*f0/(1-f0)
+
+def fun5(f0):
+    return 1/(1-f0)**2
+
+def fun6(f0):
     return 
-
-def fun5(x):
-    
-    pass
-
-def fun6(x):
-    pass
